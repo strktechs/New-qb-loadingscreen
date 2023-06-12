3fab80ec78dc3cdc052b2b34c2586c96b048ae25
strktechs <57625506+strktechs@users.noreply.github.com>
2023-06-12 20:14:14 +0530

    Add files via upload

---
+++ assets/scripts/config.js
@@ -0,0 +1,38 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "45.61.168.63:30500";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.gif", link: "https://discord.gg/WnDR6Z4sVD"},
+    {name: "tebex", label: "Donations", description: "For donations and buying Script", icon: "assets/media/icons/tebex.png", link: "https://strk.tebex.io/"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+Config.keylayer = [
+    {name: "Phone", label: "Phone", description: "To Open Phone", icon: "assets/media/icons/M.svg"},
+    {name: "Radial", label: "Radil-Menu", description: "To Open Radial Menu", icon: "assets/media/icons/F1.svg"},
+    {name: "Hud", label: "Hud", description: "To Open hud to togel hud", icon: "assets/media/icons/I.svg"},
+    {name: "Belt", label: "Seat Belt", description: "To Put seat belt on/off", icon: "assets/media/icons/B.svg"},
+    {name: "Inventory", label: "Inventory", description: "To Open inventory", icon: "assets/media/icons/TAB.svg"},
+    {name: "Vehicle Locks", label: "Vehicle Locks", description: "To Vehicle Locks/Unlock", icon: "assets/media/icons/L.svg"},
+    {name: "Toggle Engine", label: "Toggle Engine", description: "To Toggle Engine On/Off", icon: "assets/media/icons/G.svg"},
+    {name: "Open Target", label: "Open Target", description: "To Open Target", icon: "assets/media/icons/LALT.svg"},
+]  // Mobile
+
+// Staff list
+Config.Staff = [
+    {name: "lucifermorningstar", description: "Owner & Dev", color: "#00fff7", image: "https://cdn.discordapp.com/attachments/1117120934818627625/1117120980498788472/the-relation-between-the-satan-and-lucifer-the-morning-star.jpg"},
+    ];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+
+
+
