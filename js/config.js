8540aca3066dfb1fe8016aba642bbe0caf71efd4
strktechs <57625506+strktechs@users.noreply.github.com>
2023-06-12 20:13:02 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,32 @@
+
+var volume = 10
+
+let allMusic = [
+    {
+      img : 'Malvadao',
+      type_image : 'jpg',
+      name : 'Malvadão 3',
+      artist : 'Xamã',
+      src : 'song',
+      type_music : 'mp3'
+  },
+  {
+      img : 'Nemo',
+      type_image : 'jpg',
+      name : 'Nemo',
+      artist : 'JayA Luuck',
+      src : 'M2',
+      type_music : 'mp3'
+  },
+  ];
+
+  
+/*
+
+  --------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                              THANKS FOR USE MY LOADING SCREEN JOIN THE DISCORD PLEASE :(
+
+  --------------------------------------------------------------------------------------------------------------------------------------------------
+  
+*/
